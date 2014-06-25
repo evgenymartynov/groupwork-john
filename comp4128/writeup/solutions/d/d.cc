d67fa40f8df92553bc7eb3fc4369c2683ff0a4fe
John Cramb <jcramb@gmail.com>
2014-06-26 00:54:30 +1000

    got D passing local tests... but still not being accepted. tis fiddly as shit

--- comp4128/writeup/solutions/d/d.cc
+++ comp4128/writeup/solutions/d/d.cc
@@ -7,46 +7,61 @@
 
 int rows, n;
 int cols[MAX_K];
-int min[MAX_K][MAX_K];
-int max[MAX_K][MAX_K];
 int depth[MAX_K][MAX_K];
 int grid[MAX_K][MAX_K];
+int finished;
+int ans;
+
+inline int min(int x, int y) {
+  int up = (y == 0) ? 0 : grid[y-1][x];
+  int left = (x == 0) ? up + 1 : grid[y][x-1];
+  return MAX(left, up + 1);
+}
+
+inline int max(int x, int y) {
+  int up = (y == 0) ? n : grid[y-1][x];
+  int left = (x == 0) ? up + 1 : grid[y][x-1];
+  return (n - depth[y][x]);
+}
+
+int increment(int y) {
+  if (y < 0) 
+    return 0;
+
+  for (int x = cols[y] - 1; x >= 0; x--) {
+    if (grid[y][x] < max(x, y)) {
+      grid[y][x]++;
+      for (int i = x + 1; i < cols[y]; i++) {
+        grid[y][i] = min(i, y);
+      }
+      return 1;
+    }
+  }
+
+  if (y == 0) 
+    printf("finished!\n");
+  return 0;
+}
 
 void debug_print() {
   for (int y = 0; y < rows; y++) {
     for (int x = 0; x < cols[y]; x++) {
-      printf("%d-%d ", min[y][x], max[y][x]); 
+      printf("%d ", grid[y][x]); 
     }
     printf("\n");
   }
   printf("--\n");
 }
 
-int increment(int y) {
-  int x = cols[y] - 1;
-  int val = grid[y][x--];
-  while (x >= 0) {
-    if (grid[y][x] < val && grid[y][x] < max[y][x]) {
-      grid[y][x]++;
-      return 1;
-    }
-  }
-  x = cols[y] - 1;
-  if (val < max[y][x]) {
-    grid[y][x]++;
-  } else {
-    return 0;
-  }
-}
-
 int main() {
   while (scanf("%d", &rows) == 1) {
+
+    finished = 0;
     for (int y = 0; y < rows; y++) {
       scanf("%d", &cols[y]);
     }
 
     scanf("%d", &n);
-    printf("(%d)\n", n);
     memset(depth, -1, sizeof(depth));
     for (int y = 0; y < rows; y++) {
       for (int x = 0; x < cols[y]; x++) {
@@ -58,18 +73,31 @@ int main() {
 
     for (int y = 0; y < rows; y++) {
       for (int x = 0; x < cols[y]; x++) {
-        int up = (y == 0) ? 0 : min[y-1][x];
-        int left = (x == 0) ? up + 1 : min[y][x-1];
-        min[y][x] = MIN(left, up + 1);   
-        max[y][x] = n - depth[y][x];
+        grid[y][x] = min(x, y);
       }
     }
 
-    int ans = 0;
-    memcpy(grid, min, sizeof(grid));
+    ans = 1;
     debug_print();
-
-    printf("ans %d\n", ans);
+    for (int row = rows - 1; row >= 0; row--) {
+      printf("doing row %d\n", row);
+      while (increment(row)) {
+        ans++;
+        for (int y = row + 1; y < rows; y++) {
+          for (int x = 0; x < cols[y]; x++) {
+            grid[y][x] = min(x, y);
+          }
+        }
+        debug_print();
+        for (int current = rows - 1; current > row; current--) {
+          while (increment(current)) {
+            debug_print();
+            ans++;
+          }
+        }
+      }
+    }
+    printf("%d\n", ans);
   }
   return 0;
 }
